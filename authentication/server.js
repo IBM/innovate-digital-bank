a1889babb6f0a69b648096d5ea80a89be453ffa4
amalamine <amalamine00@gmail.com>
2018-04-10 13:48:11 +0400

    added mongo url via service name

--- authentication/server.js
+++ authentication/server.js
@@ -6,7 +6,10 @@ require('dotenv').config({silent: true, path: `${__dirname}/.env`});
 var server = require('./app');
 var port = 3200;
 
-mongoose.connect(process.env.MONGO_URL, function (ignore, connection) {
+let mongoUrl = process.env.NODE_ENV
+if (process.env.TOOLCHAIN_FLAG==="active") mongoUrl = `mongodb://${process.env.MONGO_USERNAME}:${MONGO_PASSWORD}@${process.env.CLUSTER_NAMESPACE}-innovate-bank-mongodb.${process.env.CLUSTER_NAMESPACE}.svc.cluster.local/${process.env.MONGO_PORT}`
+
+mongoose.connect(mongoUrl, function (ignore, connection) {
     connection.onOpen();
     server.listen(port, function () {
         console.log('Server running on port: %d', port);
