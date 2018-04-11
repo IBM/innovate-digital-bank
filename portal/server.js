b3516d07aa40439c7fcdd3c271b2d2884ca52770
amalamine <amalamine00@gmail.com>
2018-04-11 11:30:19 +0400

    reading mongourl from env variables

--- portal/server.js
+++ portal/server.js
@@ -10,10 +10,7 @@ const request = require('request');
 
 const config = require(`${__dirname}/config`)[process.env.NODE_ENV];
 
-let mongoUrl = process.env.NODE_ENV
-if (process.env.TOOLCHAIN_FLAG==="active") mongoUrl = `mongodb://${process.env.MONGO_USERNAME}:${MONGO_PASSWORD}@${process.env.CLUSTER_NAMESPACE}-innovate-bank-mongodb.${process.env.CLUSTER_NAMESPACE}.svc.cluster.local/${process.env.MONGO_PORT}`
-
-console.log('MONGO URL: ', mongoUrl)
+console.log('MONGO URL: ', process.env.MONGO_URL)
 
 var app = express();
 
@@ -23,7 +20,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(session({
     secret: process.env.SESSION_SECRET,
-    store: new mongostore({url: mongoUrl}),
+    store: new mongostore({url: process.env.MONGO_URL}),
     resave: true,
     saveUninitialized: true,
     cookie: {
@@ -44,7 +41,7 @@ require('./routes/support')(app, request, config.ports);
 
 var port = 3100;
 
-mongoose.connect(mongoUrl, function (ignore, connection) {
+mongoose.connect(process.env.MONGO_URL, function (ignore, connection) {
     connection.onOpen();
     app.listen(port, function () {
         console.log('Innovate portal running on port: %d', port);
