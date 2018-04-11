1d571adc601b0b67e33b7b4665703ae32b93b12a
amalamine <amalamine00@gmail.com>
2018-04-11 11:30:19 +0400

    reading mongourl from env variables

--- accounts/server.js
+++ accounts/server.js
@@ -6,10 +6,7 @@ require('dotenv').config({silent: true, path: `${__dirname}/.env`});
 var server = require('./app');
 var port = 3400;
 
-let mongoUrl = process.env.NODE_ENV
-if (process.env.TOOLCHAIN_FLAG==="active") mongoUrl = `mongodb://${process.env.MONGO_USERNAME}:${MONGO_PASSWORD}@${process.env.CLUSTER_NAMESPACE}-innovate-bank-mongodb.${process.env.CLUSTER_NAMESPACE}.svc.cluster.local/${process.env.MONGO_PORT}`
-
-mongoose.connect(mongoUrl, function (ignore, connection) {
+mongoose.connect(process.env.MONGO_URL, function (ignore, connection) {
     connection.onOpen();
     server.listen(port, function () {
         console.log('Server running on port: %d', port);
