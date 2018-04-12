a06f947e1fcce49d8056be35ce81664c985620f4
amalamine <amalamine00@gmail.com>
2018-04-12 11:56:20 +0400

    moved all environment variables to configmap

--- authentication/server.js
+++ authentication/server.js
@@ -6,6 +6,8 @@ require('dotenv').config({silent: true, path: `${__dirname}/.env`});
 var server = require('./app');
 var port = 3200;
 
+console.log(`Running on ${process.env.BASE_PATH}:${port}, connecting to ${process.env.MONGO_URL}`)
+
 mongoose.connect(process.env.MONGO_URL, function (ignore, connection) {
     connection.onOpen();
     server.listen(port, function () {
--- support/server.js
+++ support/server.js
@@ -5,6 +5,8 @@ require('dotenv').config({silent: true, path: `${__dirname}/.env`});
 var server = require('./app');
 var port = 4000;
 
+console.log(`Running on ${process.env.BASE_PATH}:${port}, reading from ${process.env.CONVERSTATION_WORKSPACE}`)
+
 server.listen(port, function () {
     console.log('Server running on port: %d', port);
 });
