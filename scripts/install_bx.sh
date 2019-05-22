c8573c42089dd6de75a3eecb556467e2deba7701
Oscar Gomez <50819676+termux-io@users.noreply.github.com>
2019-05-21 17:33:39 -0700

    Revert "fix travis (#13)"
    This reverts commit addb437fcaead470cefa8e9549c025de07c79453.

--- scripts/install_bx.sh
+++ scripts/install_bx.sh
@@ -5,18 +5,15 @@ if [[ "$TRAVIS_PULL_REQUEST" != "false" ]]; then
     exit 0
 fi
 
-echo "Installing IBM Cloud CLI"
-curl -L https://clis.ng.bluemix.net/download/bluemix-cli/latest/linux64 > IBM_Cloud_CLI.tar.gz
-tar -xvf IBM_Cloud_CLI.tar.gz
+echo "Installing Bluemix CLI"
+curl -L https://public.dhe.ibm.com/cloud/bluemix/cli/bluemix-cli/latest/IBM_Cloud_CLI_amd64.tar.gz > Bluemix_CLI.tar.gz
+tar -xvf Bluemix_CLI.tar.gz
 sudo ./Bluemix_CLI/install_bluemix_cli
 
-echo "IBM Cloud Developer Tools"
+echo "Installing Developer Tools"
 curl -sL https://ibm.biz/idt-installer | bash
 
-echo "Running IBM Cloud help"
-ibmcloud dev help
-
-echo "Configuring ibmcloud to disable version check"
-ibmcloud config --check-version=false
-echo "Checking ibmcloud version"
-ibmcloud --version
+echo "Configuring bx to disable version check"
+bx config --check-version=false
+echo "Checking bx version"
+bx --version
